d63b4f8023683c168c8a56f2c1030117be972038
kirasimon565 <kirasimon136@gmail.com>
2026-06-13 22:46:27 +0300

    Update animations.dart

--- lib/core/animations.dart
+++ lib/core/animations.dart
@@ -1,15 +1,31 @@
+// lib/core/animations.dart
+
 import 'package:flutter/material.dart';
-import 'constants.dart';
 
 class SparkAnimations {
+  // Curves
   static const Curve defaultCurve = Curves.easeOut;
   static const Curve sparkExpandCurve = Curves.easeOutCubic;
   static const Curve drawerCurve = Curves.easeIn;
+  static const Curve breatheCurve = Curves.easeInOut;
 
-  static Duration get fast =>
-      const Duration(milliseconds: AppConstants.durationFast);
-  static Duration get medium =>
-      const Duration(milliseconds: AppConstants.durationMedium);
-  static Duration get slow =>
-      const Duration(milliseconds: AppConstants.durationSlow);
+  // Durations
+  static const Duration fast = Duration(milliseconds: 150);
+  static const Duration medium = Duration(milliseconds: 300);
+  static const Duration slow = Duration(milliseconds: 500);
+  static const Duration breathe = Duration(milliseconds: 3500);
+  static const Duration particleBurst = Duration(milliseconds: 600);
+  static const Duration dockExpand = Duration(milliseconds: 500);
+  static const Duration searchFade = Duration(milliseconds: 200);
+  static const Duration themeChange = Duration(milliseconds: 500);
+  static const Duration eyeOpen = Duration(milliseconds: 500);
+  static const Duration eyeClose = Duration(milliseconds: 400);
+}
+
+// Backward compatibility alias for home_screen.dart
+class SparkAnimationDuration {
+  static const Duration fast = Duration(milliseconds: 150);
+  static const Duration medium = Duration(milliseconds: 300);
+  static const Duration slow = Duration(milliseconds: 500);
+  static const Duration breathe = Duration(milliseconds: 3500);
 }
